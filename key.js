2335fc2f4cd3164d7fff690c74d582618b8c42c7
simpleesarath <88574803+simpleesarath@users.noreply.github.com>
2023-12-03 03:23:07 +0530

    Delete key.js

--- key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-
-    //MongoURI:"mongodb+srv://chandu83319:WjfGPynuxsIJLZpq@cluster0.czrknd4.mongodb.net/?retryWrites=true&w=majority"
-    // MongoURI:"mongodb+srv://chandu83319:WjfGPynuxsIJLZpq@cluster0.czrknd4.mongodb.net/?retryWrites=true&w=majority"
-    MongoURI:"mongodb+srv://chandu83319:WjfGPynuxsIJLZpq@cluster0.5iwkqed.mongodb.net/"
- }
