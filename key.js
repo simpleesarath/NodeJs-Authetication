23ea7bf1c138cba5f468325770fa513cbdf4c588
simpleesarath <88574803+simpleesarath@users.noreply.github.com>
2023-12-03 02:32:07 +0530

    Add files via upload

--- key.js
+++ key.js
@@ -2,6 +2,5 @@ module.exports = {
 
     //MongoURI:"mongodb+srv://chandu83319:WjfGPynuxsIJLZpq@cluster0.czrknd4.mongodb.net/?retryWrites=true&w=majority"
     // MongoURI:"mongodb+srv://chandu83319:WjfGPynuxsIJLZpq@cluster0.czrknd4.mongodb.net/?retryWrites=true&w=majority"
-    MongoURI:"mongodb+srv://chandu83319:WjfGPynuxsIJLZpq@ac-aiz6rtp-shard-00-02.czrknd4.mongodb.net:27017,ac-aiz6rtp-shard-00-00.czrknd4.mongodb.net:27017,ac-aiz6rtp-shard-00-01.czrknd4.mongodb.net:27017/test?authSource=admin&replicaSet=atlas-6rya39-shard-0&ssl=true"
-    
+    MongoURI:"mongodb+srv://chandu83319:WjfGPynuxsIJLZpq@cluster0.5iwkqed.mongodb.net/"
  }
